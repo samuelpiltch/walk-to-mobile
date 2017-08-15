ade5826ce4e63abbbd6dfbb4d4c3af4b447670c3
samuelpiltch <samuelpiltch@gmail.com>
2017-08-14 22:26:13 -0400

    Implement incoming train feed

--- app/components/radius.js
+++ app/components/radius.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text } from 'react-native';
 
 import { Annotation } from 'react-native-mapbox-gl'
 
-export default class Stop extends Component {
+export default class Radius extends Component {
 
     render() {
         return (
@@ -12,8 +12,14 @@ export default class Stop extends Component {
                     coordinate={{ latitude: this.props.lat, longitude: this.props.long }}
                     style={{alignItems: 'center', justifyContent: 'center', position: 'absolute'}}
                 >
-                    <View style={[styles.stop, {backgroundColor: this.props.color}]}>
-                        <Text style={[styles.text, {color: this.props.textColor}]}>{this.props.line}</Text>
+                    <View
+                        style={[styles.radius, {
+                            backgroundColor: this.props.color,
+                            height: this.props.size,
+                            width: this.props.size,
+                            borderRadius: this.props.size/2
+                        }]}
+                    >
                     </View>
                 </Annotation>
         );
@@ -22,21 +28,9 @@ export default class Stop extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#e6e4e0',
-        flex: 1
-    },
-    stop: {
-        height: 20,
-        width: 20,
-        borderRadius: 10,
-        backgroundColor: 'red',
+    radius: {
+        opacity: .15,
         alignItems: 'center',
         justifyContent: 'center'
-    },
-    text: {
-        color: 'white',
-        fontSize: 14,
-        fontWeight: '800'
     }
 });
--- app/index.js
+++ app/index.js
@@ -3,9 +3,11 @@ import { StyleSheet, View } from 'react-native';
 
 import Mapbox from 'react-native-mapbox-gl';
 import { MapView } from 'react-native-mapbox-gl';
+import debounce from 'debounce';
 
 import AccessCodes from './config.js';
 import Stop from "./components/stop.js";
+import Radius from "./components/radius.js";
 
 import stops from './data/stops.js';
 
@@ -13,8 +15,148 @@ Mapbox.setAccessToken(AccessCodes.mapbox);
 
 export default class WalkTo extends Component {
 
-    render() {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            trainListArr: []
+        };
+
+        this.loadTrains = this.loadTrains.bind(this);
+    }
+
+    // Load and display incoming trains
+    loadTrains() {
+        var rawTrains = [];
+        var incomingTrainData;
+        let that = this;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost:5000', true);
+
+        // When data is pulled from server: get coords and color of coordinating stop and add incoming train to array.
+        xhr.onload = function() {
+            incomingTrainData = JSON.parse(xhr.responseText);
+
+            for (stop in incomingTrainData) {
+                var lat, long;
+
+                for (listStop of stops.data) {
+                    if (listStop[0] == stop) {
+                        lat = parseFloat(listStop[4]);
+                        long = parseFloat(listStop[5]);
+                    }
+                }
+
+                for (train of incomingTrainData[stop]) {
+                    if (incomingTrainData[stop].indexOf(train) < 3) {
+                        var stopColor;
+
+                        switch (String(stop[0]).substring(0, 1)) {
+                            case "1":
+                                stopColor = "red";
+                                break;
+                            case "2":
+                                stopColor = "red";
+                                break;
+                            case "3":
+                                stopColor = "red";
+                                break;
+                            case "4":
+                                stopColor = "green";
+                                break;
+                            case "5":
+                                stopColor = "green";
+                                break;
+                            case "6":
+                                stopColor = "green";
+                                break;
+                            case "N":
+                                stopColor = "yellow";
+                                break;
+                            case "Q":
+                                stopColor = "yellow";
+                                break;
+                            case "R":
+                                stopColor = "yellow";
+                                break;
+                            case "A":
+                                stopColor = "blue";
+                                break;
+                            case "C":
+                                stopColor = "blue";
+                                break;
+                            case "E":
+                                stopColor = "blue";
+                                break;
+                            case "B":
+                                stopColor = "orange";
+                                break;
+                            case "D":
+                                stopColor = "orange";
+                                break;
+                            case "F":
+                                stopColor = "orange";
+                                break;
+                            case "M":
+                                stopColor = "orange";
+                                break;
+                            case "G":
+                                stopColor = "lightgreen";
+                                break;
+                            case "J":
+                                stopColor = "#cd7f32";
+                                break;
+                            case "Z":
+                                stopColor = "#cd7f32";
+                                break;
+                            case "L":
+                                stopColor = "grey";
+                                break;
+                            case "S":
+                                stopColor = "grey";
+                                break;
+                            case "7":
+                                stopColor = "purple";
+                                break;
+                            case "9":
+                                stopColor = "purple";
+                                break;
+                            case "H":
+                                stopColor = "blue";
+                                break;
+                        }
+
+                        rawTrains.push(
+                            {
+                                id: stop + train.toString(),
+                                color: stopColor,
+                                lat: lat,
+                                long: long
+                            }
+                        );
+                    }
+                }
+            }
+
+            // Convert array into an array of custom React Native components.
+            var trainListArr = rawTrains.map(trainInfo => (
+                <Radius
+                    key={trainInfo.id}
+                    id={trainInfo.id}
+                    color={trainInfo.color}
+                    lat={trainInfo.lat}
+                    long={trainInfo.long}
+                    size={75}
+                />
+            ))
+            // Set array of Radius annotation to state, then placed as child of MapView.
+            that.setState({ trainListArr })
+        };
+        xhr.send();
+    }
+
+    render() {
         initStops = [];
 
         for (stop of stops.data){
@@ -140,8 +282,10 @@ export default class WalkTo extends Component {
 
                     showsUserLocation={true}
                     userTrackingMode={Mapbox.userTrackingMode.follow}
+                    onRegionDidChange={ () => {debounce(this.loadTrains(), 2000)} }
                 >
                     {stopListArr}
+                    {this.state.trainListArr}
                 </MapView>
             </View>
         );
